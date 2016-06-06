fd86583320e7b58e298fc5b8cc504b3481f9861e
harindra <harindra@broadinstitute.org>
2016-06-06 15:33:55 -0400

    back-end server now talking to reference server

--- src/main/java/org/broadinstitute/macarthurlab/beamr/matchmakers/MatchmakerSearch.java
+++ src/main/java/org/broadinstitute/macarthurlab/beamr/matchmakers/MatchmakerSearch.java
@@ -4,7 +4,9 @@
 package org.broadinstitute.macarthurlab.beamr.matchmakers;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -127,12 +129,47 @@ public class MatchmakerSearch implements Search{
 	 * @throws MalformedURLException 
 	 */
 	private List<MatchmakerResult> searchNode(Node matchmakerNode, Patient patient) {
-		System.out.println(this.callUrl(""));
 		System.out.println(matchmakerNode.getName());
 		System.out.println(matchmakerNode.getToken());
 		System.out.println(matchmakerNode.getUrl());
 		
-		
+		  HttpURLConnection connection = null;  
+		  try {
+		    //Create connection
+		    URL url = new URL(matchmakerNode.getUrl());
+		    connection = (HttpURLConnection)url.openConnection();
+		    connection.setRequestMethod("POST");
+		    connection.setRequestProperty("Content-Type","application/vnd.ga4gh.matchmaker.v1.0+json");
+		    connection.setRequestProperty("Content-Language", "en-US");  
+		    connection.setUseCaches(false);
+		    connection.setDoOutput(true);
+		    
+
+		    //Send request
+		    String payload="{\"patient\":{\"id\":\"1\",\"contact\": {\"name\":\"Jane Doe\", \"href\":\"mailto:jdoe@example.edu\"},\"features\":[{\"id\":\"HP:0000522\"}],\"genomicFeatures\":[{\"gene\":{\"id\":\"NGLY1\"}}]}}";
+		    DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+		    wr.writeBytes(payload);
+		    wr.close();
+
+		    //Get Response  
+		    java.io.InputStream is = connection.getInputStream();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		    StringBuilder response = new StringBuilder(); // or StringBuffer if not Java 5+ 
+		    String line;
+		    while((line = rd.readLine()) != null) {
+		      response.append(line);
+		      response.append('\r');
+		    }
+		    rd.close();
+		    System.out.println("--"+response.toString()+"<<<<");
+		  } catch (Exception e) {
+		    e.printStackTrace();
+		    return null;
+		  } finally {
+		    if(connection != null) {
+		      connection.disconnect(); 
+		    }
+		  }
 		
 		return new ArrayList<MatchmakerResult>();
 	}
