e5f45a2f0121bcb22f83878775758312af89e42d
harindra <harindra@broadinstitute.org>
2017-01-25 14:36:46 -0500

    Updated tests and adjusted javadocs and refactored

--- src/test/java/org/broadinstitute/macarthurlab/matchbox/AppTest.java
+++ src/test/java/org/broadinstitute/macarthurlab/matchbox/AppTest.java
@@ -13,9 +13,9 @@ import org.broadinstitute.macarthurlab.matchbox.entities.Variant;
 import org.broadinstitute.macarthurlab.matchbox.match.GenotypeMatch;
 import org.broadinstitute.macarthurlab.matchbox.match.Match;
 import org.broadinstitute.macarthurlab.matchbox.match.MatchService;
+import org.broadinstitute.macarthurlab.matchbox.match.PhenotypeMatch;
 import org.broadinstitute.macarthurlab.matchbox.matchmakers.PatientRecordUtility;
 import org.broadinstitute.macarthurlab.matchbox.metrics.Metric;
-
 import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -27,6 +27,7 @@ import junit.framework.TestSuite;
 public class AppTest extends TestCase
 {
 	private GenotypeMatch genotypeMatch = new GenotypeMatch();
+	private PhenotypeMatch phenotypeMatch = new PhenotypeMatch();
 	
     /**
      * Create the test case
@@ -68,86 +69,7 @@ public class AppTest extends TestCase
     	assertTrue(1==commonGenes.size());
         assertTrue("ENSG00000178104"==commonGenes.get(0));
     }
-    
-    
-    /**
-     * Test if a perfect match gives
-     */
-    public void testGeneTypeMatchSearchByGenomicFeatures()
-    {
-    	Patient testP1 =  getTestPatient();    	
-    	List<Patient> matches = this.genotypeMatch.searchByGenomicFeatures(testP1);
-    	Assert.assertEquals(1,matches.size());
-    }
-    	
-    	
-    /**
-     * Test if a perfect match gives back a 1 score
-     */
-    public void testPerfectMatchScore()
-    {
-    	MatchService match = new Match();
-    	Patient testP1 =  getTestPatient();  
-    	List<MatchmakerResult> scoredMatches =  match.match(testP1);
-    	double score=0.0;
-    	for (MatchmakerResult mr:scoredMatches){
-    		score=mr.getScore().get("patient");
-    	}
-    	Assert.assertEquals(1.0, score);
-    }
-    
-    
-    /**
-     * Test if empty fields get scrubbed out in Variant class
-     */
-    public void testVariantEmptyFieldScrubbing(){
-    	Patient testP1 =  getTestPatient();
-    	for (GenomicFeature gf:testP1.getGenomicFeatures()){
-    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("end"));
-    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("referenceBases"));
-    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("alternateBases"));
-    		Assert.assertEquals(42, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("start"));
-    	}	
-    }
-    
-    
-    /**
-     * Test if empty fields get scrubbed out in PhenoTypeFeature class
-     */
-    public void testPhenotypeFeatureEmptyFieldScrubbing(){
-    	Patient testP1 =  getTestPatient();
-    	for (PhenotypeFeature ff:testP1.getFeatures()){
-    		Assert.assertEquals(-1, ff.getEmptyFieldsRemovedJson().indexOf("ageOfOnset"));
-    		Assert.assertEquals(2, ff.getEmptyFieldsRemovedJson().indexOf("id"));
-    	}	
-    }
-    
-    
-    /**
-     * Test if empty fields get scrubbed out in GenoTypeFeature class
-     */
-    public void testGenotypeFeatureEmptyFieldScrubbing(){
-    	Patient testP1 =  getTestPatient();
-    	for (GenomicFeature gf:testP1.getGenomicFeatures()){
-    		Assert.assertEquals(-1, gf.getEmptyFieldsRemovedJson().indexOf("zygosity"));
-    	}	
-    }
-    
-    
-    /**
-     * Test if empty fields get scrubbed out in complete Patient entity
-     */
-    public void testPatientEmptyFieldScrubbing(){
-    	Patient testP1 =  getTestPatient();
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("species"));
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("ageOfOnset"));
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("sex"));
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("inheritanceMode"));
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("alternateBases"));
-    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("referenceBases"));
-    	Assert.assertEquals(109, testP1.getEmptyFieldsRemovedJson().indexOf("name"));
-    }
-    
+
     
     /**
      * helper: returns 2 test patients
@@ -305,6 +227,75 @@ public class AppTest extends TestCase
     	System.out.println(patient.getEmptyFieldsRemovedJson());
     }
     
+    
+
+    
+    /**
+     * Test if a perfect match gives
+     */
+    public void testGeneTypeMatchSearchByGenomicFeatures()
+    {
+    	Patient testP1 =  getTestPatient();    	
+    	List<Patient> matches = this.genotypeMatch.searchByGenomicFeatures(testP1);
+    	Assert.assertEquals(1,matches.size());
+    }
+    
+    
+    /**
+     * Test if empty fields get scrubbed out in Variant class
+     */
+    public void testVariantEmptyFieldScrubbing(){
+    	Patient testP1 =  getTestPatient();
+    	for (GenomicFeature gf:testP1.getGenomicFeatures()){
+    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("end"));
+    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("referenceBases"));
+    		Assert.assertEquals(-1, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("alternateBases"));
+    		Assert.assertEquals(42, gf.getVariant().getEmptyFieldsRemovedJson().indexOf("start"));
+    	}	
+    }
+    
+    
+    /**
+     * Test if empty fields get scrubbed out in PhenoTypeFeature class
+     */
+    public void testPhenotypeFeatureEmptyFieldScrubbing(){
+    	Patient testP1 =  getTestPatient();
+    	for (PhenotypeFeature ff:testP1.getFeatures()){
+    		Assert.assertEquals(-1, ff.getEmptyFieldsRemovedJson().indexOf("ageOfOnset"));
+    		Assert.assertEquals(2, ff.getEmptyFieldsRemovedJson().indexOf("id"));
+    	}	
+    }
+    
+    
+    
+    
+    /**
+     * Test if empty fields get scrubbed out in GenoTypeFeature class
+     */
+    public void testGenotypeFeatureEmptyFieldScrubbing(){
+    	Patient testP1 =  getTestPatient();
+    	for (GenomicFeature gf:testP1.getGenomicFeatures()){
+    		Assert.assertEquals(-1, gf.getEmptyFieldsRemovedJson().indexOf("zygosity"));
+    	}	
+    }
+    
+    
+    /**
+     * Test if empty fields get scrubbed out in complete Patient entity
+     */
+    public void testPatientEmptyFieldScrubbing(){
+    	Patient testP1 =  getTestPatient();
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("species"));
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("ageOfOnset"));
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("sex"));
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("inheritanceMode"));
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("alternateBases"));
+    	Assert.assertEquals(-1, testP1.getEmptyFieldsRemovedJson().indexOf("referenceBases"));
+    	Assert.assertEquals(109, testP1.getEmptyFieldsRemovedJson().indexOf("name"));
+    }
+    
+    
+    
     /**
      * Test counting distinct genes for metrics
      */
@@ -315,7 +306,23 @@ public class AppTest extends TestCase
     
     
     
-
-
+    /**
+     * Test if a perfect match gives back a 1 score
+     */
+    public void testPerfectMatchScore()
+    {
+    	MatchService match = new Match();
+    	
+    	Patient testP1 =  getTestPatient(); 
+ 
+    	//mocking an exact match by adding the same patient in
+    	List<Patient> matches = new ArrayList<Patient>();
+    	matches.add(testP1);
+    	
+    	List<Double> patientGenotypeRankingScores = this.genotypeMatch.rankByGenotypes(matches, testP1);
+    	List<Double> patientPhenotypeRankingScores = this.phenotypeMatch.rankByPhenotypes(matches, testP1);
+    	List<Double> scores = match.generateMergedScore(patientGenotypeRankingScores,patientPhenotypeRankingScores);
+    	Assert.assertEquals(1.0, scores.get(0));
+    }
 
 }
