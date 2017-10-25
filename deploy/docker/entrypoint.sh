e62d907358643ff0fdf77c7865e047c86b84fdb1
harindra <harindra@broadinstitute.org>
2017-10-25 14:13:59 -0400

    Better documentation and examples on how to use matchbox with docker and wire in other nodes and systems to to it. Also adding in ability to use docker matchbox as https (previously you had to go with maven build route to get https. Now exposing that in docker build version)

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -6,6 +6,32 @@ env
 
 cd /matchbox_deployment
 
-java -jar -Dallow.no-gene-in-common.matches=$ALLOW_NO_GENE_IN_COMMON_MATCHES -Dexomiser.data-directory=$EXOMISER_DATA_DIR  -Dspring.data.mongodb.host=$MONGODB_HOSTNAME  -Dspring.data.mongodb.port=$MONGODB_PORT -Dspring.data.mongodb.username=$MONGODB_USERNAME  -Dspring.data.mongodb.password=$MONGODB_PASSWORD  -Dspring.data.mongodb.database=$MONGODB_DATABASE matchbox-0.1.0.jar &
+if $USE_HTTPS
+then
+	java -jar -Dallow.no-gene-in-common.matches=$ALLOW_NO_GENE_IN_COMMON_MATCHES \
+				-Dexomiser.data-directory=$EXOMISER_DATA_DIR  \
+				-Dspring.data.mongodb.host=$MONGODB_HOSTNAME  \
+				-Dspring.data.mongodb.port=$MONGODB_PORT \
+				-Dspring.data.mongodb.username=$MONGODB_USERNAME  \
+				-Dspring.data.mongodb.password=$MONGODB_PASSWORD  \
+				-Dspring.data.mongodb.database=$MONGODB_DATABASE \
+				-Dserver.port=$SERVER_PORT \
+				-Dserver.ssl.key-store=$HTTPS_SSL_KEY_STORE \
+				-Dserver.ssl.key-store-password=$HTTPS_SSL_KEY_STORE_PASSWORD \
+				-Dserver.ssl.key-password=$HTTPS_SSL_KEY_PASSWORD \
+				matchbox-0.1.0.jar &
+else
+	java -jar -Dallow.no-gene-in-common.matches=$ALLOW_NO_GENE_IN_COMMON_MATCHES \
+				-Dexomiser.data-directory=$EXOMISER_DATA_DIR  \
+				-Dspring.data.mongodb.host=$MONGODB_HOSTNAME  \
+				-Dspring.data.mongodb.port=$MONGODB_PORT \
+				-Dspring.data.mongodb.username=$MONGODB_USERNAME  \
+				-Dspring.data.mongodb.password=$MONGODB_PASSWORD  \
+				-Dspring.data.mongodb.database=$MONGODB_DATABASE \
+				-Dserver.port=$SERVER_PORT \
+				matchbox-0.1.0.jar &
+fi
+
+
 
 sleep 10000000000
