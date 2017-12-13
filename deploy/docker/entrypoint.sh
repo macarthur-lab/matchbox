9f05e681ac05a0a182295511da47e56c138698d9
harindra <harindra@broadinstitute.org>
2017-12-13 12:58:26 -0500

    updating Dockerfile and entrypoint to reflect the new smaller exomiser data bundle and new application constant

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -19,6 +19,7 @@ then
 				-Dserver.ssl.key-store=$HTTPS_SSL_KEY_STORE \
 				-Dserver.ssl.key-store-password=$HTTPS_SSL_KEY_STORE_PASSWORD \
 				-Dserver.ssl.key-password=$HTTPS_SSL_KEY_PASSWORD \
+				-Dexomiser.phenotype.data-version=$EXOMISER_PHENOTYPE_DATA_VERSION \
 				matchbox-0.1.0.jar &
 else
 	java -jar -Dallow.no-gene-in-common.matches=$ALLOW_NO_GENE_IN_COMMON_MATCHES \
@@ -29,6 +30,7 @@ else
 				-Dspring.data.mongodb.password=$MONGODB_PASSWORD  \
 				-Dspring.data.mongodb.database=$MONGODB_DATABASE \
 				-Dserver.port=$SERVER_PORT \
+				-Dexomiser.phenotype.data-version=$EXOMISER_PHENOTYPE_DATA_VERSION \
 				matchbox-0.1.0.jar &
 fi
 
